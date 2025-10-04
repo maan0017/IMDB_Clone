bf694f29ae34f0f6f078d2cac61206aafd3f1c3e
maan0017 <robinmaan17102002@gmail.com>
2025-10-04 17:39:32 +0530

    fourth commit -fixed backend uri

--- client/src/types/axios.config.ts
+++ client/src/types/axios.config.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 // const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://imdb-clone-1-0ext.onrender.com",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
